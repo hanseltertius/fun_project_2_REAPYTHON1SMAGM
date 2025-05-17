cf05832267479688e198a3e64f5010c7d72e835c
hanseltertius <hansel.tertius@gmail.com>
2025-05-17 17:16:22 +0700

    Implement basic chat feature from OpenRouter API

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import requests
+import json
 
 st.header("💬 AI Chatbot App")
 
@@ -15,17 +17,49 @@ for message in st.session_state.messages:
 
 if message_input := st.chat_input("Input your message here"):
 
-    # TODO : need to check if the response is 200 first ya
-    with st.chat_message("user"):
-        st.markdown(message_input)
-    # TODO : only call when the response is 200, need to input first into the OpenRouter API
+    # TODO : simpan chat history untuk si user message
+
     user_payload = {
         "role": "user",
         "content": message_input
     }
+
+    with st.chat_message(user_payload.get("role")):
+        st.markdown(user_payload.get("content"))
+
     st.session_state.messages.append(user_payload)
 
-    message_response = f"Echo: {message_input}"
-    with st.chat_message("assistant"):
-        st.markdown(message_response)
-    st.session_state.messages.append({"role": "assistant", "content": message_response})
+    #region Generating the Loading Component
+    empty_space = st.empty()
+    with empty_space.container():
+        with st.status("Please wait, the AI assistant is tying a message...", expanded=True):
+            response = requests.post(
+                url="https://openrouter.ai/api/v1/chat/completions",
+                headers={
+                    "Authorization": "Bearer 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"
+                },
+                data=json.dumps({
+                    "model": "openai/gpt-3.5-turbo",
+                    "messages": [user_payload]
+                })
+            )
+    #endregion
+
+    if response:
+        empty_space.empty()
+
+        # TODO : handle error message, need to use the status along with the error message aja
+        if response.status_code == 200:
+            response_text_object = json.loads(response.text)
+
+            text_message_choices = response_text_object.get("choices")
+            if len(text_message_choices) > 0:
+                message_output = text_message_choices[0].get("message")
+                message_output_role = message_output.get("role")
+                message_output_content = message_output.get("content")
+
+                # TODO : use yield, seperti di ChatGPT
+                with st.chat_message(message_output_role):
+                    st.markdown(message_output_content)
+
+                st.session_state.messages.append({"role": message_output_role, "content": message_output_content})
