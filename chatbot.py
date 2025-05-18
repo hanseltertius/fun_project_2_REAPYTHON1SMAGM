9c3f7c151d0f44eb6a57114c0c05651b77c42fa1
hanseltertius <hansel.tertius@gmail.com>
2025-05-18 10:34:48 +0700

    Implement input image file

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,8 @@ import streamlit as st
 import requests
 import json
 import time
+import base64
+import mimetypes
 
 st.header("💬 AI Chatbot App")
 
@@ -14,19 +16,56 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 # endregion
 
-if message_input := st.chat_input("Input your message here"):
+# user_input = st.chat_input("Input your message here", accept_file=True, file_type=["jpg", "jpeg", "png"])
+user_input = st.chat_input("Input your message here", accept_file=True, file_type=["jpg", "jpeg", "png"])
 
-    # TODO : simpan chat history untuk si user message
+if user_input is not None:
+    text = user_input.get("text")
+    files = user_input.get("files")
+
+    uploaded_file = files[0] if files is not None and len(files) > 0 else None
+
+    messages = []
+
+    if text:
+        messages.append({
+            "type": "text",
+            "text": text
+        })
+    
+    # TODO : liat url / base64 nya dulu kan
+    if len(files) > 0:
+        # Determine the MIME type of the uploaded file
+        mime_type, _ = mimetypes.guess_type(uploaded_file.name)
+        
+        # Read and encode the file content to base64
+        file_content = uploaded_file.read()
+        base64_data = base64.b64encode(file_content).decode("utf-8")
+        data_url = f"data:{mime_type};base64,{base64_data}"
+
+        messages.append({
+            "type": "image_url",
+            "image_url": {
+                "url": data_url
+            }
+        })
 
     user_payload = {
         "role": "user",
-        "content": message_input
+        "content": messages
     }
 
-    with st.chat_message(user_payload.get("role")):
-        st.markdown(user_payload.get("content"))
-
-    st.session_state.messages.append(user_payload)
+    # TODO : ini jgn pake user_payload
+    with st.chat_message("user"):
+        st.markdown(text)
+        for file in files:
+            st.image(file)
+    
+    st.session_state.messages.append({
+        "role": "user",
+        "content": text,
+        "files": files
+    })
 
     #region Generating the Loading Component
     empty_space = st.empty()
@@ -38,7 +77,7 @@ if message_input := st.chat_input("Input your message here"):
                     "Authorization": f"Bearer {st.secrets["OPEN_ROUTER_API_KEY"]}"
                 },
                 data=json.dumps({
-                    "model": "openai/gpt-3.5-turbo",
+                    "model": "google/gemini-2.5-flash-preview",
                     "messages": [user_payload],
                     "stream": True
                 }),
@@ -54,7 +93,7 @@ if message_input := st.chat_input("Input your message here"):
             full_response = ""
             with st.chat_message("assistant"):
                 stream_placeholder = st.empty()
-                # TODO : get method for stream_placeholder
+                # TODO : get method for full_response
                 for line in response.iter_lines():
                     if line:
                         decoded_line = line.decode("utf-8")
@@ -65,14 +104,22 @@ if message_input := st.chat_input("Input your message here"):
                                 break
                             try:
                                 json_data = json.loads(data)
-                                choices = json_data.get("choices")
-                                if len(choices) > 0:
-                                    delta = choices[0].get("delta")
-                                    if "content" in delta:
-                                        content = delta.get("content")
-                                        full_response += content
-                                        stream_placeholder.markdown(f"{full_response}▌")
-                                        time.sleep(0.03)
+                                # TODO : get error atau tidak, ini untuk error handling nya bagaimana
+                                error = json_data.get("error")
+                                if error is not None:
+                                    st.error(f"""
+                                    **❌ Error:**\n
+                                    {error.get("message")}
+                                    """)
+                                else:
+                                    choices = json_data.get("choices")
+                                    if choices is not None and len(choices) > 0:
+                                        delta = choices[0].get("delta")
+                                        if "content" in delta:
+                                            content = delta.get("content")
+                                            full_response += content
+                                            stream_placeholder.markdown(f"{full_response}▌")
+                                            time.sleep(0.03)
                             except Exception as e:
                                 st.error(f"""
                                 **❌ Streaming Error:**\n
